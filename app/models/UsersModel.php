d48f94208c2e70e5ade6b97da091fce967d39344
David Grudl <david@grudl.com>
2009-12-17 22:22:37 +0100

    Skeleton: added LoginPresenter & UsersModel

---
+++ app/models/UsersModel.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * My Application
+ *
+ * @copyright  Copyright (c) 2009 John Doe
+ * @package    MyApplication
+ */
+
+/*use Nette\Object;*/
+/*use Nette\Security\AuthenticationException;*/
+
+
+/**
+ * Users authenticator.
+ *
+ * @author     John Doe
+ * @package    MyApplication
+ */
+class UsersModel extends Object implements /*Nette\Security\*/IAuthenticator
+{
+
+	/**
+	 * Performs an authentication
+	 * @param  array
+	 * @return IIdentity
+	 * @throws AuthenticationException
+	 */
+	public function authenticate(array $credentials)
+	{
+		$username = $credentials[self::USERNAME];
+		$password = md5($credentials[self::PASSWORD]);
+
+		$row = dibi::fetch('SELECT * FROM users WHERE login=%s', $username);
+
+		if (!$row) {
+			throw new AuthenticationException("User '$username' not found.", self::IDENTITY_NOT_FOUND);
+		}
+
+		if ($row->password !== $password) {
+			throw new AuthenticationException("Invalid password.", self::INVALID_CREDENTIAL);
+		}
+
+		unset($row->password);
+		return new /*Nette\Security\*/Identity($row->id, $row->role, $row);
+	}
+
+}
