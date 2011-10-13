95453c80b5afb17528e0d0fe5a65be6af6046d07
David Grudl <david@grudl.com>
2011-10-13 21:14:50 +0200

    maintenantce.php moved to templates

--- www/index.php
+++ www/index.php
@@ -1,8 +1,5 @@
 <?php
 
-// uncomment this line if you must temporarily take down your site for maintenance
-// require '.maintenance.php';
-
 $params = array();
 
 // absolute filesystem path to this web root
@@ -10,6 +7,9 @@ $params['wwwDir'] = __DIR__;
 
 // absolute filesystem path to the application root
 $params['appDir'] = realpath(__DIR__ . '/../app');
+
+// uncomment this line if you must temporarily take down your site for maintenance
+// require $params['appDir'] . '/templates/maintenance.phtml';
 
 // load bootstrap file
 require $params['appDir'] . '/bootstrap.php';
