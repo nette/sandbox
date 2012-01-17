46d2a00381711729cbf2ae3180ec08013ec9c254
David Grudl <david@grudl.com>
2012-01-17 05:24:28 +0100

    ErrorPresenter: more detailed log for 4xx errors

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -26,8 +26,10 @@ class ErrorPresenter extends BasePresenter
 
 		} elseif ($exception instanceof NA\BadRequestException) {
 			$code = $exception->getCode();
-			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx'); // load template 403.latte or 404.latte or ... 4xx.latte
-			Debugger::log("HTTP code $code", 'access'); // log to access.log
+			// load template 403.latte or 404.latte or ... 4xx.latte
+			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
+			// log to access.log
+			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 
 		} else {
 			$this->setView('500'); // load template 500.latte
