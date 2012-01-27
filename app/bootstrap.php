9325f366c1c23c888878899bc47393b1c7f1cd97
David Grudl <david@grudl.com>
2012-01-27 04:12:36 +0100

    updated to 2.0-rc2

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,25 +3,22 @@
 /**
  * My Application bootstrap file.
  */
-use Nette\Diagnostics\Debugger,
-	Nette\Application\Routers\Route;
+use Nette\Application\Routers\Route;
 
 
 // Load Nette Framework
 require LIBS_DIR . '/Nette/loader.php';
 
 
-// Enable Nette Debugger for error visualisation & logging
-Debugger::$logDirectory = __DIR__ . '/../log';
-Debugger::$strictMode = TRUE;
-Debugger::enable();
-
-
 // Configure application
 $configurator = new Nette\Config\Configurator;
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+// Enable Nette Debugger for error visualisation & logging
+//$configurator->setProductionMode($configurator::AUTO);
+$configurator->enableDebugger(__DIR__ . '/../log');
 
 // Enable RobotLoader - this will load all classes automatically
+$configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
 	->addDirectory(APP_DIR)
 	->addDirectory(LIBS_DIR)
@@ -31,19 +28,10 @@ $configurator->createRobotLoader()
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $container = $configurator->createContainer();
 
-// Opens already started session
-if ($container->session->exists()) {
-	$container->session->start();
-}
-
 // Setup router
-$router = $container->router;
-$router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
-$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+$container->router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
+$container->router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
 
 
 // Configure and run the application!
-$application = $container->application;
-//$application->catchExceptions = TRUE;
-$application->errorPresenter = 'Error';
-$application->run();
+$container->application->run();
