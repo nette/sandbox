d1c4485f0f209ef36faf750b361e8bb18e4c3018
David Grudl <david@grudl.com>
2012-05-29 23:27:09 +0200

    Revert "maintenantce.php moved to templates"
    This reverts commit 95453c80b5afb17528e0d0fe5a65be6af6046d07.

--- www/index.php
+++ www/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// uncomment this line if you must temporarily take down your site for maintenance
+// require '.maintenance.php';
+
 // absolute filesystem path to this web root
 define('WWW_DIR', __DIR__);
 
@@ -8,9 +11,6 @@ define('APP_DIR', WWW_DIR . '/../app');
 
 // absolute filesystem path to the libraries
 define('LIBS_DIR', WWW_DIR . '/../libs');
-
-// uncomment this line if you must temporarily take down your site for maintenance
-// require APP_DIR . '/templates/maintenance.phtml';
 
 // load bootstrap file
 require APP_DIR . '/bootstrap.php';
