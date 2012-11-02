2efee7ebfce4b6c278da060d25079da8a78462f0
David Grudl <david@grudl.com>
2012-11-02 19:04:34 +0100

    added RouterFactory

---
+++ app/router/RouterFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use Nette\Application\Routers\RouteList,
+	Nette\Application\Routers\Route,
+	Nette\Application\Routers\SimpleRouter;
+
+
+/**
+ * Router factory.
+ */
+class RouterFactory
+{
+
+	/**
+	 * @return Nette\Application\IRouter
+	 */
+	public function createRouter()
+	{
+		$router = new RouteList();
+		$router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		return $router;
+	}
+
+}
