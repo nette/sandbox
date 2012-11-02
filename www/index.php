909bf2ec523751482b487b91e873678ce5151ff1
David Grudl <david@grudl.com>
2012-11-02 19:13:52 +0100

    removed constants WWW_DIR, APP_DIR & LIBS_DIR

--- www/index.php
+++ www/index.php
@@ -3,14 +3,5 @@
 // uncomment this line if you must temporarily take down your site for maintenance
 // require '.maintenance.php';
 
-// absolute filesystem path to this web root
-define('WWW_DIR', __DIR__);
-
-// absolute filesystem path to the application root
-define('APP_DIR', WWW_DIR . '/../app');
-
-// absolute filesystem path to the libraries
-define('LIBS_DIR', WWW_DIR . '/../libs');
-
 // load bootstrap file
-require APP_DIR . '/bootstrap.php';
+require __DIR__ . '/../app/bootstrap.php';
