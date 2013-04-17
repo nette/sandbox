1a4c5e082599b7e7d241f095d72d221b259b3368
David Grudl <david@grudl.com>
2013-04-17 23:40:00 +0200

    updated netteForms.js, autoload.php, jQuery & Adminer

--- libs/autoload.php
+++ libs/autoload.php
@@ -3,7 +3,8 @@
 // This is default autoload.php. It can be overwritten by Composer.
 
 if (!is_file(__DIR__ . '/Nette/loader.php')) {
-	die("Nette Framework is expected in directory '" . __DIR__ . "/Nette' but not found. Check if the path is correct or edit file '" . __FILE__ . "'.");
+	echo("Nette Framework is expected in directory '" . __DIR__ . "/Nette' but not found. Edit file '" . __FILE__ . "' or execute `composer update`.");
+	exit(1);
 }
 
 require __DIR__ . '/Nette/loader.php';
