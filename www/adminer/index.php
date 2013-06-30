b0715da306439f2ed121cea28678f8272e88c0a2
David Grudl <david@grudl.com>
2013-07-01 01:01:41 +0200

    Adminer is accessible only from localhost

--- www/adminer/index.php
+++ www/adminer/index.php
@@ -1,5 +1,13 @@
 <?php
 
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) ||
+	!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+{
+	header('HTTP/1.1 403 Forbidden');
+	exit;
+}
+
+
 $root = __DIR__ . '/../../libs/dg/adminer-custom';
 
 if (!is_file($root . '/index.php')) {
@@ -8,29 +16,27 @@ if (!is_file($root . '/index.php')) {
 }
 
 
-if (isset($_GET['file'])) {
-	$file = $_GET['file'];
-	if (preg_match('#^(?:static/)?[\w.-]+\.(\w+)$#', $file, $m) && is_file("$root/$file")) {
-
-		if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-			header('HTTP/1.1 304 Not Modified');
-			exit;
-		}
-
-		header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime('1 month')) . ' GMT');
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-
-		if ($m[1] === 'css') {
-			header('Content-Type: text/css; charset=utf-8');
-		} elseif ($m[1] === 'js') {
-			header('Content-Type: text/javascript; charset=utf-8');
-		} elseif ($m[1] === 'gif' || $m[1] === 'png' ||$m[1] === 'jpg') {
-			header("Content-Type: image/$m[1]");
-		}
-		include "$root/$file";
+if (isset($_GET['file']) && preg_match('#^(?:static/)?[\w.-]+\.(\w+)$#', $_GET['file'], $m)
+	&& is_file("$root/$_GET[file]"))
+{
+	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+		header('HTTP/1.1 304 Not Modified');
 		exit;
 	}
+
+	header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime('1 month')) . ' GMT');
+	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+	if ($m[1] === 'css') {
+		header('Content-Type: text/css; charset=utf-8');
+	} elseif ($m[1] === 'js') {
+		header('Content-Type: text/javascript; charset=utf-8');
+	} elseif ($m[1] === 'gif' || $m[1] === 'png' ||$m[1] === 'jpg') {
+		header("Content-Type: image/$m[1]");
+	}
+	readfile("$root/$_GET[file]");
+	exit;
 }
 
 
-include $root . '/index.php';
+require $root . '/index.php';
