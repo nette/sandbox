38580eae48d91b607b33da38b7fde95a506f1b11
David Grudl <david@grudl.com>
2013-12-25 21:22:39 +0100

    Adminer: added message "Adminer is available only from localhost"

--- www/adminer/index.php
+++ www/adminer/index.php
@@ -4,6 +4,8 @@ if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) |
 	!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
 {
 	header('HTTP/1.1 403 Forbidden');
+	echo 'Adminer is available only from localhost';
+	for ($i = 2e3; $i; $i--) echo substr(" \t\r\n", rand(0, 3), 1);
 	exit;
 }
 
