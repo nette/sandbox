40d9b8eb17cbc269b56ec725a57a64dfb07d53d8
David Grudl <david@grudl.com>
2013-12-27 02:16:49 +0100

    removed comments from bootstrap.php

--- www/index.php
+++ www/index.php
@@ -3,8 +3,6 @@
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require '.maintenance.php';
 
-// Let bootstrap create Dependency Injection container.
 $container = require __DIR__ . '/../app/bootstrap.php';
 
-// Run application.
 $container->getService('application')->run();
