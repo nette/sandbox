a1d02425ecec9d6f5576d3bfeb80bd1161a8e607
David Grudl <david@grudl.com>
2014-01-18 04:35:33 +0100

    added bin/create-user.php

---
+++ bin/create-user.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!isset($_SERVER['argv'][2])) {
+	echo '
+Add new user to database.
+
+Usage: create-user.php <name> <password>
+';
+	exit(1);
+}
+
+list(, $user, $password) = $_SERVER['argv'];
+
+$container = require __DIR__ . '/../app/bootstrap.php';
+$container->getByType('Model\UserManager')->add($user, $password);
+
+echo "User $user was added.\n";
