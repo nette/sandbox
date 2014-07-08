c17f31e9943f9b5bc8da1a7db347a551fec1d889
David Grudl <david@grudl.com>
2014-07-09 00:30:39 +0200

    ErrorPresenter: logger is passed via constructor

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -4,7 +4,7 @@ namespace App\Presenters;
 
 use Nette,
 	App\Model,
-	Tracy\Debugger;
+	Tracy\ILogger;
 
 
 /**
@@ -12,6 +12,15 @@ use Nette,
  */
 class ErrorPresenter extends BasePresenter
 {
+	/** @var ILogger */
+	private $logger;
+
+
+	public function __construct(ILogger $logger)
+	{
+		$this->logger = $logger;
+	}
+
 
 	/**
 	 * @param  Exception
@@ -24,11 +33,11 @@ class ErrorPresenter extends BasePresenter
 			// load template 403.latte or 404.latte or ... 4xx.latte
 			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
 			// log to access.log
-			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
+			$this->logger->log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 
 		} else {
 			$this->setView('500'); // load template 500.latte
-			Debugger::log($exception, Debugger::EXCEPTION); // and log exception
+			$this->logger->log($exception, ILogger::EXCEPTION); // and log exception
 		}
 
 		if ($this->isAjax()) { // AJAX request? Note this error in payload.
