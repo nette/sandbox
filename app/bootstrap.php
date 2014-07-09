138bf7e5d70f71cee80ebc482344494b9c7daa2e
David Grudl <david@grudl.com>
2014-07-09 16:44:07 +0200

    $configurator->setDebugMode(TRUE) changed to remote IP

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode(TRUE);  // debug mode MUST NOT be enabled on production server
+//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
