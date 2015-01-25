b2dd15fa1d62d5e9eec836f25df9c7097ffa73cf
David Grudl <david@grudl.com>
2015-01-25 14:51:26 +0100

    small fixes

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -28,7 +28,7 @@ class ErrorPresenter extends BasePresenter
 
 		} else {
 			$this->setView('500'); // load template 500.latte
-			Debugger::log($exception, Debugger::ERROR); // and log exception
+			Debugger::log($exception, Debugger::EXCEPTION); // and log exception
 		}
 
 		if ($this->isAjax()) { // AJAX request? Note this error in payload.
