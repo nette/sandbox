abdd156350186355767f957561d2662a4d9e07f7
David Grudl <david@grudl.com>
2015-04-01 18:59:50 +0200

    composer: autoload requires to install Nette via Composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (!class_exists('Tester\Assert')) {
-	echo "Install Nette Tester using `composer update --dev`\n";
-	exit(1);
-}
-
 Tester\Environment::setup();
 
 $configurator = new Nette\Configurator;
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,10 +1,6 @@
 <?php
 
-// This is default autoload.php. It can be overwritten by Composer.
+// This is default autoload.php. It must be overwritten by Composer.
 
-if (!is_file(__DIR__ . '/nette/nette/Nette/loader.php')) {
-	echo("Nette Framework is expected in directory '" . __DIR__ . "/Nette' but not found. Edit file '" . __FILE__ . "' or execute `composer update`.");
-	exit(1);
-}
-
-require __DIR__ . '/nette/nette/Nette/loader.php';
+echo 'Install Nette using `composer install`';
+exit(1);
