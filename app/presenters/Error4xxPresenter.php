8a23c7e371b5b5a83e7e7565c76a0af99b370509
David Grudl <david@grudl.com>
2015-11-29 18:27:18 +0100

    Error4xxPresenter: is achievable only via forward

--- app/presenters/Error4xxPresenter.php
+++ app/presenters/Error4xxPresenter.php
@@ -8,7 +8,16 @@ use Nette;
 class Error4xxPresenter extends BasePresenter
 {
 
-	public function renderDefault(\Exception $exception)
+	public function startup()
+	{
+		parent::startup();
+		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
+			$this->error();
+		}
+	}
+
+
+	public function renderDefault(Nette\Application\BadRequestException $exception)
 	{
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
