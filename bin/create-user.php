6b466ea3ea26067967cd920a73013838292f8eb3
Martin Zlámal <mrtnzlml@gmail.com>
2016-06-10 20:28:52 +0200

    create-user.php: fix compatibility with UserManager

--- bin/create-user.php
+++ bin/create-user.php
@@ -4,18 +4,19 @@ if (!isset($_SERVER['argv'][2])) {
 	echo '
 Add new user to database.
 
-Usage: create-user.php <name> <password>
+Usage: create-user.php <name> <email> <password>
 ';
 	exit(1);
 }
 
-list(, $name, $password) = $_SERVER['argv'];
+list(, $name, $email, $password) = $_SERVER['argv'];
 
 $container = require __DIR__ . '/../app/bootstrap.php';
+/** @var App\Model\UserManager $manager */
 $manager = $container->getByType(App\Model\UserManager::class);
 
 try {
-	$manager->add($name, $password);
+	$manager->add($name, $email, $password);
 	echo "User $name was added.\n";
 
 } catch (App\Model\DuplicateNameException $e) {
