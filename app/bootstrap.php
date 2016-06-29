e3fecd67761123d4be278aaedf52570f41caf713
David Grudl <david@grudl.com>
2016-06-29 21:34:25 +0200

    used enableTracy()

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->enableDebugger(__DIR__ . '/../log');
+$configurator->enableTracy(__DIR__ . '/../log');
 
 $configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
