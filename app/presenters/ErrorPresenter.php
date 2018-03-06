526f603142591bbd937c141710b0d1eb35d4ab50
David Grudl <david@grudl.com>
2018-03-06 11:11:26 +0100

    ErrorPresenter: renders 500.phtml only in HTML

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -5,6 +5,7 @@ namespace App\Presenters;
 
 use Nette;
 use Nette\Application\Responses;
+use Nette\Http;
 use Tracy\ILogger;
 
 
@@ -37,8 +38,10 @@ class ErrorPresenter implements Nette\Application\IPresenter
 		}
 
 		$this->logger->log($e, ILogger::EXCEPTION);
-		return new Responses\CallbackResponse(function () {
-			require __DIR__ . '/templates/Error/500.phtml';
+		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse) {
+			if (preg_match('#^text/html(?:;|$)#', $httpResponse->getHeader('Content-Type'))) {
+				require __DIR__ . '/templates/Error/500.phtml';
+			}
 		});
 	}
 }
