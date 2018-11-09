fb14ab4523c5be525e6c4e89dcb0e02ccc3dd4f5
Milan Matějček <milan.matejcek@gmail.com>
2018-11-09 10:42:16 +0100

    Example how use interface for factory

--- app/forms/FormFactory.php
+++ app/forms/FormFactory.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace App\Forms;
 
-use Nette;
 use Nette\Application\UI\Form;
 
 
-final class FormFactory
+interface FormFactory
 {
-	public function create(): Form
-	{
-		$form = new Form;
-		return $form;
-	}
+	function create(): Form;
 }
