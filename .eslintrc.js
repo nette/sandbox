375c72a5ad5693b1417665758d20c48b563b765c
David Grudl <david@grudl.com>
2019-04-09 18:40:40 +0200

    added .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	"parserOptions": {
+		"ecmaVersion": 6,
+		"sourceType": "module"
+	},
+	"env": {
+		"browser": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
