6108f641dab0ec13fca8df3f9fc8e1a0308ff132
David Grudl <david@grudl.com>
2020-06-08 22:24:38 +0200

    setDebugMode secret@ip

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -13,7 +13,7 @@ class Bootstrap
 	{
 		$configurator = new Configurator;
 
-		//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
 		$configurator->enableTracy(__DIR__ . '/../log');
 
 		$configurator->setTimeZone('Europe/Prague');
