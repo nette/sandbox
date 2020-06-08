7343c871e53964fa98d40f341a804fa22601494e
David Grudl <david@grudl.com>
2020-06-08 22:48:24 +0200

    index.php: removed fluent interface

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-App\Bootstrap::boot()
-	->createContainer()
-	->getByType(Nette\Application\Application::class)
-	->run();
+$configurator = App\Bootstrap::boot();
+$container = $configurator->createContainer();
+$application = $container->getByType(Nette\Application\Application::class);
+$application->run();
