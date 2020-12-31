aea4f8585f5f859d865873c7a0fb36a5782275f9
David Grudl <david@grudl.com>
2020-12-31 01:51:59 +0100

    config moved to root

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -12,20 +12,21 @@ class Bootstrap
 	public static function boot(): Configurator
 	{
 		$configurator = new Configurator;
+		$appDir = dirname(__DIR__);
 
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy(__DIR__ . '/../log');
+		$configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory(__DIR__ . '/../temp');
+		$configurator->setTempDirectory($appDir . '/temp');
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
 		$configurator
-			->addConfig(__DIR__ . '/config/common.neon')
-			->addConfig(__DIR__ . '/config/local.neon');
+			->addConfig($appDir . '/config/common.neon')
+			->addConfig($appDir . '/config/local.neon');
 
 		return $configurator;
 	}
